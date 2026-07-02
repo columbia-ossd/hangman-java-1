9cb689cb1f6d4e5a113858cf3ed27ddee23dd6d7
Byeolah-Kwon <byeolahk@gmail.com>
2026-07-02 11:36:11 -0400

    implment dictioary word selection

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Dictionary {
-    
+    private static final String[] WORDS = {
+        "Seattle", "Spokane", "Tacoma", "Vancouver", "Bellevue", "Kent",
+        "Everett", "Renton", "Yakima", "Kirkland", "Bellingham", "Auburn",
+        "Redmond", "Lakewood", "Shoreline", "Olympia", "Bothell", "Edmonds",
+        "Lynnwood", "Pullman", "Mukilteo", "Monroe", "Snoqualmie",
+        "Leavenworth", "Arlington", "Brier", "Snohomish"
+    };
+
+    private static final Random RANDOM = new Random();
+
+    public String getRandomWord() {
+        return WORDS[RANDOM.nextInt(WORDS.length)];
+    }
+
+    public String getCategory() {
+        return "Washington State cities";
+    }
 }
