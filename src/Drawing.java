dd23440d463b6a2ae1e810f4dad5f117130f59a7
Byeolah-Kwon <byeolahk@gmail.com>
2026-07-02 11:41:57 -0400

    add drawing stages

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,3 +1,81 @@
 public class Drawing {
-    
+    private static final String[] STAGES = {
+        """
+        _________
+        |
+        |
+        |
+        |
+        |
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |
+        |
+        |
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |       |
+        |
+        |
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |     --|
+        |
+        |
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |     --|--
+        |
+        |
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |     --|--
+        |      /
+        |     /
+        |_______________________
+        """,
+
+        """
+        _________
+        |       |
+        |       O
+        |     --|--
+        |      / \
+        |     /   \
+        |_______________________
+        """
+    };
+
+    public static String getDrawing(int numberOfErrors) {
+        if (numberOfErrors < 0 || numberOfErrors >= STAGES.length) {
+            throw new IllegalArgumentException("Invalid number of errors: " + numberOfErrors);
+        }
+
+        return STAGES[numberOfErrors];
+    }
 }
